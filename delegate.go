d2d855071e0df462eabe7d6a278c539698b6bab7
DebuggerAndrzej <redpandaworkstation@gmail.com>
2023-08-04 20:45:29 +0200

    Add dummy list item manipulation
    Add dummy list manipulation:
    - remove element
    - print which one was selected

--- delegate.go
+++ delegate.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -15,9 +16,35 @@ func (i item) FilterValue() string { return "" }
 
 type itemDelegate struct{}
 
-func (d itemDelegate) Height() int                             { return 10 }
-func (d itemDelegate) Spacing() int                            { return 0 }
-func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Height() int  { return 10 }
+func (d itemDelegate) Spacing() int { return 0 }
+func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd {
+	keys := newDelegateKeyMap()
+	var title string
+
+	if i, ok := m.SelectedItem().(Issue); ok {
+		title = i.Title()
+	} else {
+		return nil
+	}
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, keys.choose):
+			return m.NewStatusMessage(statusMessageStyle("You chose " + title))
+
+		case key.Matches(msg, keys.remove):
+			index := m.Index()
+			m.RemoveItem(index)
+			if len(m.Items()) == 0 {
+				keys.remove.SetEnabled(false)
+			}
+			return m.NewStatusMessage(statusMessageStyle("Deleted " + title))
+		}
+	}
+	return nil
+}
 func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
 	i, ok := listItem.(Issue)
 
@@ -35,4 +62,22 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 	}
 
 	fmt.Fprint(w, fn(str))
+}
+
+type delegateKeyMap struct {
+	choose key.Binding
+	remove key.Binding
+}
+
+func newDelegateKeyMap() *delegateKeyMap {
+	return &delegateKeyMap{
+		choose: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "choose"),
+		),
+		remove: key.NewBinding(
+			key.WithKeys("x", "backspace"),
+			key.WithHelp("x", "delete"),
+		),
+	}
 }
--- lipgloss_styles.go
+++ lipgloss_styles.go
@@ -3,7 +3,10 @@ package main
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	appStyle          = lipgloss.NewStyle().Border(lipgloss.RoundedBorder(), true).Padding(1, 5, 1, 5)
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	appStyle           = lipgloss.NewStyle().Border(lipgloss.RoundedBorder(), true).Padding(1, 5, 1, 5)
+	itemStyle          = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle  = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	statusMessageStyle = lipgloss.NewStyle().
+				Foreground(lipgloss.AdaptiveColor{Light: "#04B575", Dark: "#04B575"}).
+				Render
 )
